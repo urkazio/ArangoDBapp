78806a93aaeccef48bcd2d080807d40838deca70
urko garcia <62657313+urkazio@users.noreply.github.com>
2022-11-27 17:56:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,19 @@
 from pyArango.connection import *
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def crearInsertar():
-
-    conn = Connection(username="root", password="root_passwd")  # conectarse a arangodb
-
-    # crear base de datos y coleccion
-    self.db = conn.createDatabase(name="AS2022")
-    self.db = conn["AS2022"]
-    studentsCollection = self.db.createCollection(name="Estudiantes")
-    self.db["Estudiantes"]
 
 
-    # crear documento y añadir clave
-    doc = coleccion.createDocument()
-    doc["nombre"] = "Urko"
-    doc["apellido"] = "Garcia"
-    doc["edad"] = "20"
-    doc._key = "urkogarcia"
-    doc.save()
+conn = Connection(arangoURL="http://34.76.56.173:8529/", username="root", password="password")
 
-    # crear y ejecutar la query AQL
-    bind = {"doc": doc}
-    aql = "INSERT @doc INTO Estudiantes LET newDoc = NEW RETURN newDoc"
-    queryResult = self.db.AQLQuery(aql, bindVars=bind)
-    queryResult[0]  # comprobar si se ha insertado correctamente
-    print("Todo OK!")
+# crear base de datos y coleccion
+db = conn.createDatabase(name="AS2022")
+db = conn["AS2022"]
+Students = db.createCollection(name="Students")
+db["Students"]
+
+# crear y ejecutar la query AQL
+doc = {'_key': 'denisdiderot', 'name': 'Denis Diderot', 'gpa': 3.7}
+bind = {"doc": doc}
+aql = "INSERT @doc INTO Students LET newDoc = NEW RETURN newDoc"
+queryResult = db.AQLQuery(aql, bindVars=bind)
+
+print(queryResult[0])
+print(Students["denisdiderot"])

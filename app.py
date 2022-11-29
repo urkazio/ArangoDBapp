f224ac8efeeb5e290812a0e4c1b46fe912f4c68e
urko garcia <62657313+urkazio@users.noreply.github.com>
2022-11-29 18:39:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from pyArango.connection import *
+import time
 
-
+time.sleep(15)
 conn = Connection(arangoURL="http://34.76.56.173:8529/", username="root", password="password")
 
 # crear base de datos y coleccion
@@ -14,6 +15,3 @@ doc = {'_key': 'denisdiderot', 'name': 'Denis Diderot', 'gpa': 3.7}
 bind = {"doc": doc}
 aql = "INSERT @doc INTO Students LET newDoc = NEW RETURN newDoc"
 queryResult = db.AQLQuery(aql, bindVars=bind)
-
-print(queryResult[0])
-print(Students["denisdiderot"])

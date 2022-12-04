550b9de2399154df373e71ff63b32243908a070e
urko garcia <62657313+urkazio@users.noreply.github.com>
2022-12-04 13:58:42 +0100

    Update app.py

--- 3ra aplicacion/app.py
+++ 3ra aplicacion/app.py
@@ -1,5 +1,6 @@
 from pyArango.connection import *
 import time
+from datetime import datetime
 
 time.sleep(15)
 conn = Connection(arangoURL="http://34.76.56.173:8529/", username="root", password="password")
@@ -15,3 +16,10 @@ doc = {'_key': 'denisdiderot', 'name': 'Denis Diderot', 'gpa': 3.7}
 bind = {"doc": doc}
 aql = "INSERT @doc INTO Students LET newDoc = NEW RETURN newDoc"
 queryResult = db.AQLQuery(aql, bindVars=bind)
+
+# guardar en log para mostrar en web
+now = datetime.now()
+time = now.strftime("%H:%M:%S")
+a = open('logs.txt','a')
+a.write('\n' +time+ ' ' +aql)
+a.close()
